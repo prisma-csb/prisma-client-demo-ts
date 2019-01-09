a783f5096865f15111134dcd67450a830ae42be5
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-09 17:51:45 +0100

    add chalk

--- README.md
+++ README.md
@@ -19,5 +19,3 @@ There are three folders with scripts that you can use to play around with the Pr
 - ⭐️ src/more-data-access
 
 **⚠️ IMPORTANT:** Database writes are disabled by default! Please fork this CodeSandbox and use your own Prisma Demo server to perform database writes. Visit [the docs](www.prisma.io/docs/get-started) to learn how to setup a Prisma Demo server.
-
-> PS. Feel free to hide the browser that CodeSandbox opens by default - you don't need it as you're just running terminal scripts.
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { exec } from 'child_process'
 import { checkWritesEnabled } from './helpers/checkWritesEnabled'
 import { determineScript } from './helpers/determineScript'
+import chalk from 'chalk'
 
 async function main() {
 
@@ -8,11 +9,13 @@ async function main() {
   const writesEnabled = await checkWritesEnabled()
   const script = await determineScript(writesEnabled)
   const command = `yarn ${script}`
+  const file = `${script}.ts`
 
   // Run command
-  console.log(`\$ ${command}`)
+  console.log(`\nRunning command: ${chalk.bold.blue(`${command}`)} ...\n`)
   exec(command, (err, stdout) => {
     console.log(stdout)
+    console.log(`\nCheck out ${chalk.bold.blue(file)} to learn more.\n`)
   })
 
 }
