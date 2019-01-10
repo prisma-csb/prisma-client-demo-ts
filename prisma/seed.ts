081630b25f01b1bb78f4dbb161324830e13a44d5
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-10 10:13:29 +0100

    minor adjustments

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,95 +1,95 @@
-import { prisma } from "./generated/prisma-client";
+import { prisma } from '../src/generated/prisma-client'
 
 async function run() {
   const ada = await prisma.createUser({
-    name: "Ada",
-    email: "ada@prisma.io",
-    role: "ADMIN",
+    name: 'Ada',
+    email: 'ada@prisma.io',
+    role: 'ADMIN',
     posts: {
       create: [
         {
-          title: "Introducing the Analytical Engine"
+          title: 'Introducing the Analytical Engine',
         },
         {
-          title: "Building General-Purpose Computers",
-          published: true
+          title: 'Building General-Purpose Computers',
+          published: true,
         },
         {
-          title: "RESTful APIs Considered Harmful"
+          title: 'RESTful APIs Considered Harmful',
         },
         {
-          title: "Why Algorithms are Awesome",
-          published: true
-        }
-      ]
-    }
-  });
+          title: 'Why Algorithms are Awesome',
+          published: true,
+        },
+      ],
+    },
+  })
 
   const grace = await prisma.createUser({
-    name: "Grace",
-    email: "grace@prisma.io",
+    name: 'Grace',
+    email: 'grace@prisma.io',
     posts: {
       create: [
         {
-          title: "Five Things You Didn't Know About Compilers"
+          title: "Five Things You Didn't Know About Compilers",
         },
         {
-          title: "GraphQL - The Query Language of the Future",
-          published: true
+          title: 'GraphQL - The Query Language of the Future',
+          published: true,
         },
         {
-          title: "Progamming with English Words",
+          title: 'Progamming with English Words',
           published: true,
           comments: {
             create: {
-              text: "Great work!",
+              text: 'Great work!',
               writtenBy: {
                 connect: {
-                  email: "ada@prisma.io"
-                }
-              }
-            }
-          }
-        }
-      ]
-    }
-  });
+                  email: 'ada@prisma.io',
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
+  })
 
   const lynn = await prisma.createUser({
-    name: "Lynn",
-    email: "lynn@prisma.io",
+    name: 'Lynn',
+    email: 'lynn@prisma.io',
     posts: {
       create: [
         {
-          title: "Working at Xerox PARC"
+          title: 'Working at Xerox PARC',
         },
         {
-          title: "Introduction to VLSI Systems",
+          title: 'Introduction to VLSI Systems',
           published: true,
           comments: {
             create: [
               {
-                text: "I love this.",
+                text: 'I love this.',
                 writtenBy: {
                   connect: {
-                    email: "ada@prisma.io"
-                  }
-                }
+                    email: 'ada@prisma.io',
+                  },
+                },
               },
               {
-                text: "Very interesting!",
+                text: 'Very interesting!',
                 writtenBy: {
                   connect: {
-                    email: "grace@prisma.io"
-                  }
-                }
-              }
-            ]
-          }
-        }
-      ]
-    }
-  });
+                    email: 'grace@prisma.io',
+                  },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  })
 }
 
-run().catch(e => console.error(e));
+run().catch(e => console.error(e))
