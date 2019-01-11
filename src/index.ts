dcd22e586cf3dee541715d5a9fa86390a1221f53
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-11 16:20:23 +0100

    minor adjustments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { determineScript } from './helpers/determineScript'
 import { execute } from './helpers/execute'
 
+/**
+ * 🚀 Run `yarn start` to start exploring the API. 
+ * 
+ * 💡 To explore all available features, fork the CodeSandbox and connect it to your own demo database.
+ * Follow the instructions here: https://github.com/prisma-csb/prisma-client-demo-ts/blob/master/SETUP.md
+ */
+
 async function main() {
   // Determine which script to run by launching an interactive wizard
   const script = await determineScript()
