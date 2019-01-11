aa9e46ef7c6f5efa5adaf61256042216d8ca41df
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-11 11:48:53 +0100

    minor adjustments

--- src/helpers/determineScript.ts
+++ src/helpers/determineScript.ts
@@ -5,6 +5,7 @@ import {
   moreChoices,
 } from './choices'
 import * as inquirer from 'inquirer'
+import chalk from 'chalk'
 import { checkWritesEnabled } from './checkWritesEnabled'
 
 export async function determineScript() {
@@ -25,13 +26,13 @@ export async function determineScript() {
   } else if (initialAnswer.theme === initialChoices[1]) {
     // Writing data
     if (!writesEnabled) {
-      console.log(`You need to fork this Sandbox and regenerate the Prisma client with your own database to perform writes. \nMore info here: TODO`)
+      console.log(`You need to fork this Sandbox and regenerate the Prisma client with your own database to perform writes. \nFind the setup instructions here: ${chalk.bold(`https://github.com/prisma-csb/prisma-client-demo-js/blob/master/SETUP.md`)}`)
       process.exit()
     }
     finalChoices = writingChoices
   } else if (initialAnswer.theme === initialChoices[2]) {
     // More
-    console.log(`You need to fork this Sandbox and regenerate the Prisma client with your own database to explore these features. \nMore info here: TODO`)
+    console.log(`You need to fork this Sandbox and regenerate the Prisma client with your own database to explore these features. \nFind the setup instructions here: ${chalk.bold(`https://github.com/prisma-csb/prisma-client-demo-js/blob/master/SETUP.md`)}`)
     process.exit()
     finalChoices = moreChoices
   } else {
